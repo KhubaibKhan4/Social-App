0cbcaaea46d04548c7c45e3134c12562e5cb7d4b
KhubaibKhan4 <18.bscs.803@gmail.com>
2023-03-20 16:09:28 +0500

    Social App

--- app/src/main/java/com/codespacepro/loginappfirebase/Fragments/ProfileFragment.java
+++ app/src/main/java/com/codespacepro/loginappfirebase/Fragments/ProfileFragment.java
@@ -2,13 +2,23 @@ package com.codespacepro.loginappfirebase.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.codespacepro.loginappfirebase.Models.Users;
 import com.codespacepro.loginappfirebase.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class ProfileFragment extends Fragment {
@@ -19,11 +29,44 @@ public class ProfileFragment extends Fragment {
     }
 
 
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+    TextView username, fullname, email, gender, dob;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
+
+        username = view.findViewById(R.id.username_profile);
+        fullname = view.findViewById(R.id.fullname_profile);
+        email = view.findViewById(R.id.email_profile);
+        gender = view.findViewById(R.id.gender_profile);
+        dob = view.findViewById(R.id.dob_profile);
+
+        myRef.child("Users").child(FirebaseAuth.getInstance().getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Users users = snapshot.getValue(Users.class);
+
+                username.setText(users.getUsername());
+                fullname.setText(users.getFullname());
+                email.setText(users.getEmail());
+                gender.setText(users.getGender());
+                dob.setText(users.getDob());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getContext(), "" + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        return view;
     }
 }
